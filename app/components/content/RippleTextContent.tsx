d431799a0945f97c8a7a72ba0aa577acb3e3bb59
Chase Christensen <chaserchristensen@gmail.com>
2025-11-06 13:23:03 -0700

    Update RippleTextContent component with new SVG background and improved styles; add new yellow background images for enhanced visual presentation.

--- app/components/content/RippleTextContent.tsx
+++ app/components/content/RippleTextContent.tsx
@@ -16,33 +16,32 @@ export default function RippleTextContent() {
             primitiveUnits="userSpaceOnUse"
           >
             <feImage
-              href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='200' height='200'%3E%3Cdefs%3E%3ClinearGradient id='g' x1='0%25' y1='0%25' x2='0%25' y2='100%25'%3E%3Cstop offset='0%25' stop-color='%23000000'/%3E%3Cstop offset='25%25' stop-color='%23808080'/%3E%3Cstop offset='50%25' stop-color='%23ffffff'/%3E%3Cstop offset='75%25' stop-color='%23808080'/%3E%3Cstop offset='100%25' stop-color='%23000000'/%3E%3C/linearGradient%3E%3C/defs%3E%3Crect width='200' height='200' fill='%23808080'/%3E%3Cpath d='M 0 100 Q 50 50 100 100 T 200 100' stroke='url(%23g)' stroke-width='100' fill='none'/%3E%3C/svg%3E"
+              href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='900' height='900'%3E%3Cdefs%3E%3CradialGradient id='g' cx='50%25' cy='50%25' r='50%25' fx='50%25' fy='50%25'%3E%3Cstop offset='0%25' stop-color='%23000000'/%3E%3Cstop offset='25%25' stop-color='%23808080'/%3E%3Cstop offset='40%25' stop-color='%23ffffff'/%3E%3Cstop offset='75%25' stop-color='%23808080'/%3E%3Cstop offset='100%25' stop-color='%23000000'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect width='900' height='900' fill='%23808080'/%3E%3Cpath d='M 0 450 Q 225 225 450 450 T 900 450' stroke='url(%23g)' stroke-width='450' fill='none'/%3E%3C/svg%3E"
               result="waveSource"
             />
-            <feGaussianBlur in="waveSource" stdDeviation="4" result="blurred" />
             <feComponentTransfer in="blurred" result="verticalWave">
-              <feFuncR type="discrete" tableValues="0.5" />
+              <feFuncR type="discrete" tableValues=".5" />
               <feFuncG type="identity" />
             </feComponentTransfer>
             <feDisplacementMap
               in="SourceGraphic"
               in2="verticalWave"
-              scale="50"
+              scale="40"
               xChannelSelector="R"
               yChannelSelector="G"
             />
           </filter>
         </defs>
       </svg>
-      <div className="relative w-[clamp(200px,80%,800px)] mx-auto py-10">
-        <div className="space-y-16 leading-none relative text-[3vw] bg-fixed bg-linear-to-b font-serif  font-[150] from-transparent via-white to-transparent bg-clip-text text-white/50">
+      <div className="relative w-[clamp(200px,80%,900px)] mx-auto py-10">
+        <div className="space-y-16 leading-none relative text-[2vw] bg-fixed bg-linear-to-b font-serif  font-[150] from-transparent via-white to-transparent bg-clip-text text-white/50">
           <p
-            className="text-[1.5em] text-balance font-mono uppercase bg-clip-text text-transparent"
+            className="text-[1.25em] text-balance font-mono font-bold uppercase bg-clip-text text-transparent"
             style={{
               filter: "url(#rippleDisplacement)",
-              backgroundImage: "url(/img/no-bg-cropped.png)",
-              backgroundSize: "100%",
-              backgroundPosition: "left top",
+              backgroundImage: "url(/img/yellowbg-lg.jpg)",
+              backgroundSize: "900px",
+              backgroundPosition: "center top",
               backgroundAttachment: "fixed",
             }}
           >
