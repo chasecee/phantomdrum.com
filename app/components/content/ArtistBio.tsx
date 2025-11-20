2cf3f488bdfde3e0be3d5f361558a37b091ec7b6
Chase Christensen <chaserchristensen@gmail.com>
2025-11-19 19:00:04 -0700

    Refactor ArtistBio component to use section element for improved semantics and styling; add background SVG for enhanced visual presentation.

--- app/components/content/ArtistBio.tsx
+++ app/components/content/ArtistBio.tsx
@@ -1,26 +1,34 @@
 export default function ArtistBio() {
   return (
-    <div
-      className="text-[clamp(1.2rem,5vw,2.3rem)] 
-    [&>p]:leading-[1.45] [&>p]:text-pretty my-[10vw] font-serif  space-y-12 px-20 pr-10 max-w-[40ch] mx-auto"
+    <section
+      className="relative mx-auto w-full  px-20 pr-10"
+      style={{
+        backgroundImage: "url('/img/scroll.svg')",
+        backgroundSize: "100% 100%",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundBlendMode: "difference",
+        containerType: "inline-size",
+      }}
     >
-      <p>
-        <strong>Phantom Drum</strong> is built on moody synth layers, chopped
-        vocals, organic funk patterns and dusty records.
-      </p>
-      <p>
-        Ten songs, meticulously composed, embark on a search for the line
-        between chaos and comfort.
-      </p>
-      <p>
-        How many layers can a dang beat have? INITIALIZE seeks to answer this
-        question through samples, synthesizers and drum machines.
-      </p>
-
-      <p>
-        Curiosity drives the production style from Gorillaz to Enya to Aphex
-        Twin.
-      </p>
-    </div>
+      <div className="relative text-[clamp(1.2rem,2cqi,2.3rem)] max-w-[30ch] p-[5cqi] mx-auto [&>p]:leading-[1.45] [&>p]:text-pretty font-serif space-y-12">
+        <p>
+          <strong>Phantom Drum</strong> is built on moody synth layers, chopped
+          vocals, organic funk patterns and dusty records.
+        </p>
+        <p>
+          Ten songs, meticulously composed, embark on a search for the line
+          between chaos and comfort.
+        </p>
+        <p>
+          How many layers can a dang beat have? INITIALIZE seeks to answer this
+          question through samples, synthesizers and drum machines.
+        </p>
+        <p>
+          Curiosity drives the production style from Gorillaz to Enya to Aphex
+          Twin.
+        </p>
+      </div>
+    </section>
   );
 }
